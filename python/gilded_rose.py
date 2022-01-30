4780c3501378c0cafd811dc9570798cc3756ba84
stevewardle <steve_wardle@hotmail.co.uk>
2022-01-30 15:55:28 +0000

    Move logic out of `GildedRose` class and split up the two aspects of updating into their own methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,54 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+class ItemUpdater:
+    def __init__(self):
+        pass
+
+    def update(self, item):
+        self.update_sell_in(item)
+        self.update_quality(item)
+
+    def update_sell_in(self, item):
+       if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
+
+    def update_quality(self, item):
+        if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 10:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 5:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+ 
+        if item.sell_in < 0:
+            if item.name != "Aged Brie":
+                if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1        
+
+class GildedRose:
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item_updater = ItemUpdater()
+            item_updater.update(item)
 
 
 class Item:
