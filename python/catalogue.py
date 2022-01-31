b979bde748616c76606cb1d7a53d53d8bd5a967d
stevewardle <steve_wardle@hotmail.co.uk>
2022-01-31 21:07:28 +0000

    Tidy up classes and add Conjured item type

--- python/catalogue.py
+++ python/catalogue.py
@@ -3,16 +3,23 @@ from item_updater import (
     BasicItemUpdater,
     MaturingItemUpdater,
     EventItemUpdater,
-    LegendaryItemUpdater)
+    LegendaryItemUpdater,
+    ConjuredItemUpdater,
+)
 
 # In a real world application this information could be stored
-# in some sort of configuration; a file; a database or something
+# in some sort of configuration; a file, a database or something
 # else that could be easily updated for any items that already
 # fit one of the predefined updater types
 
 UPDATER_MAPPING = defaultdict(BasicItemUpdater)
 UPDATER_MAPPING.update({
-    "Aged Brie": MaturingItemUpdater(),
-    "Backstage passes to a TAFKAL80ETC concert": EventItemUpdater(),
-    "Sulfuras, Hand of Ragnaros": LegendaryItemUpdater(),
+    "Aged Brie":
+        MaturingItemUpdater(),
+    "Backstage passes to a TAFKAL80ETC concert":
+        EventItemUpdater(hype_thresholds=[10, 5]),
+    "Sulfuras, Hand of Ragnaros":
+        LegendaryItemUpdater(),
+    "Conjured Mana Cake":
+        ConjuredItemUpdater(),
     })
--- python/item_updater.py
+++ python/item_updater.py
@@ -2,13 +2,17 @@ from abc import ABC, abstractmethod
 
 
 class ItemUpdater(ABC):
+    ITEM_QUALITY_DECAY_RATE = 1
+    ITEM_QUALITY_MAX = 50
+    ITEM_QUALITY_MIN = 0
+    ITEM_SELL_IN_DECAY_RATE = 1
+
     def update(self, item):
         self.update_sell_in(item)
         self.update_quality(item)
 
-    @abstractmethod
     def update_sell_in(self, item):
-        pass
+        item.sell_in -= self.ITEM_SELL_IN_DECAY_RATE
 
     @abstractmethod
     def update_quality(self, item):
@@ -16,25 +20,30 @@ class ItemUpdater(ABC):
 
 
 class BasicItemUpdater(ItemUpdater):
-    def update_sell_in(self, item):
-        item.sell_in -= 1
-
     def update_quality(self, item):
-        if item.quality > 0:
-            item.quality -= 1
-        if item.sell_in < 0 and item.quality > 0:
-            item.quality -= 1
+        if item.quality > self.ITEM_QUALITY_MIN:
+            item.quality -= self.ITEM_QUALITY_DECAY_RATE
+        if (item.sell_in < 0
+                and item.quality > self.ITEM_QUALITY_MIN):
+            item.quality -= self.ITEM_QUALITY_DECAY_RATE
+
+
+class ConjuredItemUpdater(ItemUpdater):
+    def update_quality(self, item):
+        if item.quality > self.ITEM_QUALITY_MIN:
+            item.quality -= self.ITEM_QUALITY_DECAY_RATE*2
+        if (item.sell_in < 0
+                and item.quality > self.ITEM_QUALITY_MIN):
+            item.quality -= self.ITEM_QUALITY_DECAY_RATE*2
 
 
 class MaturingItemUpdater(ItemUpdater):
-    def update_sell_in(self, item):
-        item.sell_in -= 1
-
     def update_quality(self, item):
-        if item.quality < 50:
-            item.quality += 1
-        if item.sell_in < 0 and item.quality < 50:
-            item.quality += 1
+        if item.quality < self.ITEM_QUALITY_MAX:
+            item.quality += self.ITEM_QUALITY_DECAY_RATE
+        if (item.sell_in < 0
+                and item.quality < self.ITEM_QUALITY_MAX):
+            item.quality += self.ITEM_QUALITY_DECAY_RATE
 
 
 class LegendaryItemUpdater(ItemUpdater):
@@ -46,15 +55,19 @@ class LegendaryItemUpdater(ItemUpdater):
 
 
 class EventItemUpdater(ItemUpdater):
-    def update_sell_in(self, item):
-        item.sell_in -= 1
+    def __init__(self, hype_thresholds=[]):
+        # The thresholds are the days remaining before the event
+        # at which the increase in value becomes greater
+        self.hype_thresholds = hype_thresholds
 
     def update_quality(self, item):
-        if item.quality < 50:
-            item.quality += 1
-            if item.sell_in < 10 and item.quality < 50:
-                item.quality += 1
-            if item.sell_in < 5 and item.quality < 50:
-                item.quality += 1
+        if item.quality < self.ITEM_QUALITY_MAX:
+            item.quality += self.ITEM_QUALITY_DECAY_RATE
+            # Increase the value an additional time for each
+            # threshold we have already passed
+            for hype in self.hype_thresholds:
+                if (item.sell_in < hype
+                        and item.quality < self.ITEM_QUALITY_MAX):
+                    item.quality += self.ITEM_QUALITY_DECAY_RATE
         if item.sell_in < 0:
-            item.quality = 0
+            item.quality = self.ITEM_QUALITY_MIN
